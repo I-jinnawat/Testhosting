848effb8d6c1a0e35d8a12fe15f3b61b3854cdec
Piyapat Inphook <6431503095@lamduan.mfu.ac.th>
2024-06-28 08:28:34 +0700

    up

--- Controllers/document.js
+++ Controllers/document.js
@@ -1,42 +1,69 @@
 const Document = require('../Models/document');
+const { bucket } = require('../Config/multer');
+
+const uploadFile = async (file) => {
+  const originalname = file.originalname;
+  const blob = bucket.file(`${Date.now()}-${originalname}`);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+    gzip: true,
+  });
+
+  return new Promise((resolve, reject) => {
+    blobStream.on('error', (err) => {
+      reject(err);
+    });
+
+    blobStream.on('finish', async () => {
+      await blob.makePublic();
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      resolve(publicUrl);
+    });
+
+    blobStream.end(file.buffer);
+  });
+};
 
 exports.list = async (req, res) => {
   try {
+    const documents = await Document.find();
     res.render('document', {
       userLoggedIn: !!req.session.user,
       user: req.session.user,
+      documents,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'Internal Server Error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 exports.read = async (req, res) => {
   try {
     const category = req.params.category;
-    const documents = await Document.find({category: category}).exec();
+    const documents = await Document.find({ category });
     res.json(documents);
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'Internal server error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
 exports.display_edit_page = async (req, res) => {
   try {
-    const id = req.params.id; // Extracting ID from req.params
-    const document = await Document.findById(id); // Finding the document by ID
+    const id = req.params.id;
+    const document = await Document.findById(id);
     if (!document) {
-      return res.status(404).json({error: 'Document not found'});
+      return res.status(404).json({ error: 'Document not found' });
     }
     res.render('edit-document', {
       document,
-      userLoggedIn: true,
+      userLoggedIn: !!req.session.user,
       user: req.session.user,
-    }); // Rendering an edit page with document data
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'Internal server error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
@@ -52,37 +79,36 @@ exports.create = async (req, res) => {
       attachmentType,
     } = req.body;
 
-    let attachment = null;
-    if (attachmentType === 'file') {
-      // Get the file path of the uploaded file
-      attachment = req.files['attachment']
-        ? req.files['attachment'][0].path
-        : null;
+    let fileUrl = null;
+    let imageUrl = null;
+
+    if (attachmentType === 'file' && req.files && req.files['attachment']) {
+      fileUrl = await uploadFile(req.files['attachment'][0]);
     } else if (attachmentType === 'link') {
-      // Get the URL of the link
-      attachment = req.body.attachmentLink;
-      res.send(attachment);
+      fileUrl = req.body.attachment;
     }
 
-    // res.send('test2');
-    const imagePath = req.files['image'] ? req.files['image'][0].path : null;
+    if (req.files && req.files['image']) {
+      imageUrl = await uploadFile(req.files['image'][0]);
+    }
 
-    // Create new document entry in the database
-    const document = await Document.create({
+    const document = new Document({
       category,
       title,
-      file: attachmentType === 'file' ? attachment : null,
-      link: attachmentType === 'link' ? attachment : null,
+      file: attachmentType === 'file' ? fileUrl : null,
+      link: attachmentType === 'link' ? fileUrl : null,
       adminName,
       numberID,
       organization,
       role,
-      image: imagePath,
+      image: imageUrl,
     });
 
+    await document.save();
     res.status(201).redirect('/document');
   } catch (error) {
-    res.status(500).json({error: error.message});
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
@@ -94,7 +120,7 @@ exports.display_add_page = async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'Internal Server Error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
@@ -111,60 +137,53 @@ exports.update = async (req, res) => {
       attachmentType,
     } = req.body;
 
-    let attachment = null;
-    if (attachmentType === 'file') {
-      attachment = req.files['attachment']
-        ? req.files['attachment'][0].path
-        : null;
-      // res.send(attachment);
+    let fileUrl = null;
+    let imageUrl = null;
+
+    if (attachmentType === 'file' && req.files && req.files['attachment']) {
+      fileUrl = await uploadFile(req.files['attachment'][0]);
     } else if (attachmentType === 'link') {
-      attachment = req.body.attachment;
-      // res.send(attachment);
+      fileUrl = req.body.attachment;
     }
 
-    const imagePath = req.files['image'] ? req.files['image'][0].path : null;
-
-    const updateData = {};
-    if (category) updateData.category = category;
-    if (title) updateData.title = title;
-
-    if (attachmentType === 'file' && attachment) {
-      updateData.file = attachment;
-      updateData.link = null;
-    } else if (attachmentType === 'link' && attachment) {
-      updateData.link = attachment;
-      updateData.file = null;
+    if (req.files && req.files['image']) {
+      imageUrl = await uploadFile(req.files['image'][0]);
     }
 
-    if (adminName) updateData.adminName = adminName;
-    if (numberID) updateData.numberID = numberID;
-    if (organization) updateData.organization = organization;
-    if (role) updateData.role = role;
-    if (imagePath) updateData.image = imagePath;
+    const updateData = {
+      category,
+      title,
+      file: attachmentType === 'file' ? fileUrl : null,
+      link: attachmentType === 'link' ? fileUrl : null,
+      adminName,
+      numberID,
+      organization,
+      role,
+      image: imageUrl,
+    };
 
-    const updatedDocument = await Document.findByIdAndUpdate(id, updateData, {
-      new: true,
-    });
+    const updatedDocument = await Document.findByIdAndUpdate(id, updateData, { new: true });
 
     if (!updatedDocument) {
-      return res.status(404).json({error: 'Document not found'});
+      return res.status(404).json({ error: 'Document not found' });
     }
 
     res.status(200).redirect('/document');
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: 'Internal server error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 exports.remove = async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
 
   try {
     await Document.findByIdAndDelete(id);
     res.sendStatus(204);
   } catch (error) {
     console.error('Error deleting Document:', error);
-    res.status(500).json({error: 'Internal Server Error'});
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
